24126ebb372c85b4a8f93f48b8005ad530c3c5f9
naveena duggirala <deepikaduggirala7@gmail.com>
2024-04-11 15:06:28 -0700

    UI Improvements

--- flamingo_ui/app.js
+++ flamingo_ui/app.js
@@ -1,9 +1,20 @@
+const API_ENDPOINT_PLAYER_REGISTRATION = 'https://m97t449xz5.execute-api.us-west-2.amazonaws.com/dev/playerRegistration';
+const API_ENDPOINT_GET_CARD = 'https://m97t449xz5.execute-api.us-west-2.amazonaws.com/dev/getCard';
+const API_ENDPOINT_FLAMINGO_SIGHTING = 'https://m97t449xz5.execute-api.us-west-2.amazonaws.com/dev/flamingoSightingSubmission';
+const API_KEY = 'dummy_key';
+
 document.addEventListener('DOMContentLoaded', function() {
-    const form = document.getElementById('registrationForm');
-    if (form) {
-        form.addEventListener('submit', handleFormSubmission);
+    const registrationForm = document.getElementById('registrationForm');
+    if (registrationForm) {
+        registrationForm.addEventListener('submit', handleFormSubmission);
     }
-    
+
+    const gameContainer = document.getElementById('gameContainer');
+    if (gameContainer) {
+        gameContainer.addEventListener('click', handleCardItemClick);
+    }
+
+    initializeGame();
 });
 
 function handleFormSubmission(event) {
@@ -13,95 +24,82 @@ function handleFormSubmission(event) {
     const dataSource = document.getElementById('dataSource').value;
 
     if (dataSource === 'mock') {
-        // Redirect with mock data source
         redirectToGamePage(email, 'mock');
     } else {
-        // Actual call to register API
-        console.log(`Registering user with email: ${email} and name: ${userName}`);
-        
-        // API Gateway endpoint
-        const apiEndpoint = 'https://your-api-gateway-endpoint/endpoint';
-        
-        // Request body
-        const requestBody = {
-            email: email,
-            name: userName  
-        };
-        
-        // Fetch options
-        const fetchOptions = {
-            method: 'POST',
-            headers: {
-                'x-api-key': apiKey,
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(requestBody)
-        };
-        
-        // Performing the fetch request
-        fetch(apiEndpoint, fetchOptions)
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error('Network response was not ok');
-                }
-                return response.json();
-            })
-            .then(data => {
-                console.log('Registration successful:', data);
-                // On success, redirect to game page with customer data source
-                redirectToGamePage(email, 'customer');
-            })
-            .catch(error => {
-                console.error('Registration failed:', error);
-                // Handle registration failure, e.g., show an error message to the user
-            });
+        registerPlayer(email, userName);
     }
 }
 
-
-function redirectToGamePage(email, dataSource) {
-    window.location.href = `game.html?email=${encodeURIComponent(email)}&dataSource=${dataSource}`;
+function handleCardItemClick(event) {
+    if (event.target.classList.contains('cardItem')) {
+        const selectedText = event.target.textContent;
+        if (selectedText !== 'Free') {
+            triggerFlamingoSighting(selectedText);
+        }
+    }
 }
 
-document.addEventListener('DOMContentLoaded', function() {
+function initializeGame() {
     const dataSource = document.getElementById('dataSource');
-    const gameContainer = document.getElementById('gameContainer');
-
-    // Initialize game based on query parameters or default to mock
     const queryParams = new URLSearchParams(window.location.search);
     const email = queryParams.get('email');
     const dataSourceValue = queryParams.get('dataSource') || 'mock';
     dataSource.value = dataSourceValue;
 
-    // Fetch and display data based on the selected data source
     fetchAndDisplayData(dataSourceValue, email);
+}
 
+function registerPlayer(email, userName) {
+    const requestBody = {
+        email_id: email,
+        name: userName
+    };
 
+    const fetchOptions = {
+        method: 'POST',
+        headers: {
+            'x-api-key': API_KEY,
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(requestBody)
+    };
 
-   
-});
+    fetch(API_ENDPOINT_PLAYER_REGISTRATION, fetchOptions)
+        .then(response => {
+            if (!response.ok) {
+                if (response.status === 409) {
+                    alert('Player already exists. Please try with a different email or username.');
+                } else {
+                    alert('Please try again');
+                }
+            }
+            return response.json();
+        })
+        .then(data => {
+            redirectToGamePage(email, 'customer');
+        })
+        .catch(error => {
+            console.error('Registration failed:', error);
+        });
+}
+
+function redirectToGamePage(email, dataSource) {
+    window.location.href = `game.html?email=${encodeURIComponent(email)}&dataSource=${dataSource}`;
+}
 
 function fetchAndDisplayData(source, email) {
     if (source === 'mock') {
         const response = getMockData();
         displayData(response);
     } else {
-        // Define your API Gateway endpoint and API key
-        const apiEndpoint = 'https://m97t449xz5.execute-api.us-west-2.amazonaws.com/dev/getCard';
-        const apiKey = 'dummy_api_key';
-        
-        const urlWithParams = `${apiEndpoint}?email=${encodeURIComponent(email)}`;
-
-        // const urlWithParams = 'https://e62zz2leyg.execute-api.us-east-1.amazonaws.com/foo/card/bingo@inspiringapps.com'
-        const acceptHeader = 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7';
+        const urlWithParams = `${API_ENDPOINT_GET_CARD}?email=${encodeURIComponent(email)}`;
 
         fetch(urlWithParams, {
             method: 'GET',
             headers: {
-                'x-api-key': apiKey,
+                'x-api-key': API_KEY,
                 'Content-Type': 'application/json'
             }
-            
         })
         .then(response => {
             if (!response.ok) {
@@ -119,9 +117,7 @@ function fetchAndDisplayData(source, email) {
     }
 }
 
-
 function getMockData() {
-    // Mock data structure, similar to the provided example
     return  {
         card: [
             "South America",
@@ -159,10 +155,42 @@ function displayData(data) {
     });
 }
 
-function submitSelection(source, email) {
-    console.log('Submitting selection for', email);
-    // For the mock path, just simulate an action like refreshing the displayed data
-    if (source === 'mock') {
-        alert('Mock submission complete.');
-    }
+function triggerFlamingoSighting(selectedText) {
+    const requestBody = {
+        region: selectedText
+    };
+
+    const fetchOptions = {
+        method: 'POST',
+        headers: {
+            'x-api-key': API_KEY,
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(requestBody)
+    };
+
+    fetch(API_ENDPOINT_FLAMINGO_SIGHTING, fetchOptions)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then(data => {
+            console.log('API call successful:', data);
+            markRegionAsFilled(selectedText);
+            alert('Region marked as filled successfully!');
+        })
+        .catch(error => {
+            console.error('API call failed:', error);
+        });
+}
+
+function markRegionAsFilled(selectedText) {
+    const cardItems = document.querySelectorAll('.cardItem');
+    cardItems.forEach(cardItem => {
+        if (cardItem.textContent === selectedText) {
+            cardItem.classList.add('selected');
+        }
+    });
 }
